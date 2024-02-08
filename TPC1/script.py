06c0e0f58c066dd08fecd49086b011779f538651
afonsoabc36 <silva.af36@gmail.com>
2024-02-08 00:26:11 +0000

    Progress on the scripts

--- TPC1/script.py
+++ TPC1/script.py
@@ -1,5 +1,7 @@
 import xml.etree.ElementTree as ET
+import shutil
 import os
+import re
 
 html = """
 <!DOCTYPE html>
@@ -14,26 +16,42 @@ html = """
 
 template = html
 
-# Criar a lista de ruas a partir do nome dos ficheiros
-# Pegar na lista de ruas e fazer o índice
-# Criar os links para cada uma das páginas html deles
-# \t para fazer ytab, \n para new line
-# os.mkdir("ruas")
+html += "\t<ul>\n"
+
+# Verificar se existe a diretoria e elimina a mesma
+if os.path.exists("htmlRuas"):
+    shutil.rmtree("htmlRuas")
+os.mkdir("htmlRuas")
 
 # Caminho até aos ficheiros xml
 directoryPath = '../../MapaRuas-materialBase/texto/'
 
+listaRuas = []
+
 for filename in os.listdir(directoryPath):
+    nomeRuaSemEspacos = filename[7:-4]
+    nomeRua = ' '.join(re.findall('[A-Z][^A-Z]*', filename[7:-4])) # Remover o MRB-XX e o .xml
+    listaRuas.append(nomeRua)
     filePath = os.path.join(directoryPath, filename)
     
     tree = ET.parse(filePath)
     root = tree.getroot()
 
-    # Criar página html para cada uma delas
+    # Criar página html para cada uma delas com conteúdo
     templateRua = template
-    #template += 
+    #templateRua +=
+    #templateRua += "</body>\n"
+    #templateRua += "</html>\n" 
 
-    #write(ruas/...)
+    #fileCidade = open(f"htmlRuas/{nomeRuaSemEspacos}.html", "w", encoding="utf-8")
+    #fileCidade.write(templateRua)
+    #fileCidade.close()
+
+for elem in listaRuas: # ou sorted(listaRuas):
+    nomeSemEspaços = elem.replace(" ","")
+    html += f"\t\t<li><a href='html/{nomeSemEspaços}.html'>{elem}</a></li>\n"
+
+html += "\t<ul>\n"
 
 # html += "</body>\n"
 # html += "</html>"
