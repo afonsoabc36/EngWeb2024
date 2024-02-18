9b3830d1a84b9595357ad4b426efa19a7ffa696b
afonsoabc36 <silva.af36@gmail.com>
2024-02-18 18:53:31 +0000

    code done :)

--- TPC1/script.py
+++ TPC1/script.py
@@ -3,8 +3,7 @@ import shutil
 import os
 import re
 
-html = """
-<!DOCTYPE html>
+html = """<!DOCTYPE html>
 <html>
 <head>
     <title>TPC1 - EngWeb2024</title>
@@ -15,6 +14,8 @@ html = """
 
 template = html
 
+html += "\t<h1>Mapa Virtual</h1>\n"
+html += "\t<h2>Índice</h2>\n"
 html += "\t<ul>\n"
 
 # Verificar se existe a diretoria e elimina a mesma caso exista
@@ -25,7 +26,7 @@ os.mkdir("htmlRuas")
 # Caminho até aos ficheiros xml, encontram-se 2 diretorias a cima
 directoryPath = '../../MapaRuas-materialBase/texto/'
 
-listaRuas = []
+listaRuas = {}
 
 # Processa o texto e destaca os elementos necessários
 def processText(elem):
@@ -48,7 +49,6 @@ def processText(elem):
 for filename in os.listdir(directoryPath):
     nomeRuaSemEspacos = filename[7:-4]
     nomeRua = ' '.join(re.findall('[A-Z][^A-Z]*', filename[7:-4])) # Remover o MRB-XX e o .xml
-    listaRuas.append(nomeRua)
     filePath = os.path.join(directoryPath, filename)
     
     tree = ET.parse(filePath)
@@ -61,39 +61,54 @@ for filename in os.listdir(directoryPath):
     # Criar página html para cada uma delas com conteúdo
     templateRua = template
     numeroRua = meta.find('número').text
-    templateRua += f"\t<h1>Rua {numeroRua}: {meta.find('nome').text}</h1>\n"
+    listaRuas[int(numeroRua)] = nomeRua
+
+    templateRua += f"\t<h1>Rua {numeroRua}: {meta.find('nome').text}</h1>\n\t<hr />\n"
+    # Descrição
     templateRua += f"\t<h2>Descrição</h2>\n"
     for paragrafos in corpo.findall('para'):
         texto = processText(paragrafos)
         templateRua += f"\t<p>{texto}</p>\n"
-    templateRua += f"\t<h2>Imagens</h2>\n"
+
+    # Imagens
+    templateRua += f"\t<p></p>\n\t<hr />\n\t<h2>Imagens</h2>\n\t<h3>Atuais</h3>"
+    # Atuais
+    directoryFotoAtual = "../../MapaRuas-materialBase/atual/"
+    for filename in os.listdir(directoryFotoAtual):
+        numeroRuaHifen = numeroRua + "-"
+        if filename.startswith(numeroRuaHifen):
+            templateRua += f"\t<img src='../../../MapaRuas-materialBase/atual/{filename}' style='max-height: 400px;'>\n"
+    # Antigas
+    templateRua += "\t<h3>Antigas</h3>"
     for figura in corpo.findall('figura'):
         figuraPath = figura.find('imagem').get('path')[3:]
         templateRua += f"\t<img src='../../../MapaRuas-materialBase/{figuraPath}' style='max-height: 400px;'>\n"
         templateRua += f"\t<p>{figura.find('legenda').text}</p>\n"
-    directoryFotoAtual = "../../MapaRuas-materialBase/atual/"
-    for filename in os.listdir(directoryFotoAtual):
-        if numeroRua in filename:
-            print(filename)
-            templateRua += f"\t<img src='../../../MapaRuas-materialBase/atual/{filename}' style='max-height: 400px;'>\n"
-    templateRua += f"\t<h2>Lista de casas</h2>\n"
+    
+    # Lista de Casas
+    templateRua += f"\t<p></p>\n\t<hr />\n\t<h2>Lista de casas</h2>\n"
     if listaCasas is not None:
         for casa in listaCasas.findall('casa'):
             casa_numero = casa.find('número').text if casa.find('número') is not None else "N/A"
             enfiteuta = casa.find('enfiteuta').text if casa.find('enfiteuta') is not None else "N/A"
             foro = casa.find('foro').text if casa.find('foro') is not None else "N/A"
+            vista = casa.find('vista').text if casa.find('vista') is not None else "N/A"
             if casa.find('desc') is not None:
                 desc = processText(casa.find('desc'))
             else:
                 desc = "N/A"
 
-            templateRua += f"\t<p><b>Casa número: </b> {casa_numero}</p>\n"
+            templateRua += f"\t<h4>Casa número {casa_numero}</h4>\n"
             templateRua += f"\t<p><b>Enfiteuta: </b> {enfiteuta}</p>\n"
             templateRua += f"\t<p><b>Foro: </b> {foro}</p>\n"
-            templateRua += f"\t<p><b>Descrição: </b> {desc}</p>\n"
+            templateRua += f"\t<p><b>Vista: </b> {vista}</p>\n"
+            templateRua += f"\t<p><b>Descrição: </b> {desc}</p>\n\t<p></p>\n\t<hr />\n"
     else:
         templateRua += "<p>N/A</p>\n"
-    templateRua += f"\t<h5><a href='../ruasBraga.html'>Voltar</a><h5>\n"
+    
+    # Voltar
+    templateRua += f"\t<h4><a href='../ruasBraga.html'>Voltar</a><h5>\n"
+    
     templateRua += "</body>\n"
     templateRua += "</html>"
 
@@ -101,9 +116,10 @@ for filename in os.listdir(directoryPath):
     fileCidade.write(templateRua)
     fileCidade.close()
 
-for elem in listaRuas: # ou sorted(listaRuas):
-    nomeSemEspaços = elem.replace(" ","")
-    html += f"\t\t<li><a href='htmlRuas/{nomeSemEspaços}.html'>{elem}</a></li>\n"
+# Adicionar ruas ao indíce ordenadas pelo número de rua
+for number , name in sorted(listaRuas.items()):
+    nomeSemEspaços = name.replace(" ","")
+    html += f"\t\t<li><a href='htmlRuas/{nomeSemEspaços}.html'>{name}</a></li>\n"
 
 html += "\t<ul>\n"
 
