ab8d3de47c30c7bdcb2a580514d75c41f1fbfd3f
afonsoabc36 <silva.af36@gmail.com>
2024-03-04 22:19:52 +0000

    Almost done

---
+++ TPC3/script.py
@@ -0,0 +1,34 @@
+import json
+
+movies = []
+genres = set()
+cast = set()
+
+with open('filmes.json', 'r') as jsonFile:
+    for line in jsonFile:
+        line = json.loads(line)
+
+        lineCast = line.get('cast', [])
+        lineGenres = line.get('genres', [])
+
+        movieData = {
+            'id' : line['_id']['$oid'],
+            'title' : line['title'],
+            'year' : line['year'],
+            'cast' : lineCast,
+            'genres' : lineGenres
+        }
+        movies.append(movieData)
+
+        cast.update(lineCast)
+        genres.update(lineGenres)
+
+finalStructure = {
+    'movies' : movies,
+    'genres' : list(genres),
+    'cast' : list(cast)
+}
+    
+
+with open('filmesModified.json', 'w') as jsonFile:
+    json.dump(finalStructure, jsonFile, indent=2)
